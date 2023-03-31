68c0585b580dada72d07416a8f728ca521e98aee
Betsegaw Sebsibe <betsesebe@gmail.com>
2023-03-31 10:57:39 +0300

    Add fix to desktop design

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import './App.css';
-import bgMob from './assets/pattern-bg-mobile.png';
 import arrow from './assets/icon-arrow.svg';
-import bgDesk from './assets/pattern-bg-desktop.png';
 function App() {
-  // const imageUrl = window.innerWidth >= 375 ? bgDesk : bgMob;
-
   return (
-    <div className="w-full space-y-8 bg-no-repeat h-96 bg-mobileBG md:bg-desktopBG">
-      <h1 className="pt-8 text-3xl font-semibold text-center text-white">
+    <div className="w-full bg-no-repeat h-96 bg-mobileBG md:bg-desktopBG bg-cover">
+      <h1 className="pt-10 text-3xl font-semibold text-center text-white">
         IP Address Tracket
       </h1>
-      <div className="flex ml-6 md:justify-center">
-        <div className="px-5 py-3 bg-white cursor-pointer rounded-l-2xl md:w-[40%]">
+      <div className="flex ml-6 mt-20 justify-center">
+        <div className="px-5 py-3 bg-white cursor-pointer rounded-l-2xl w-[70%] md:w-[40%]">
           <input
             className="text-lg  outline-none text-[#2b2b2b] md:placeholder:text-lg w-full placeholder:hidden"
             placeholder="Search for any IP adresses or domain"
@@ -21,32 +17,36 @@ function App() {
           <img src={arrow} />
         </button>
       </div>
-      <div className="absolute flex flex-col md:flex-row md:space-x-10 px-16 md:px-10 md:py-12 py-5 space-y-5 md:space-y-0 bg-white shadow-lg rounded-2xl top-40 left-[5%] md:left-[20%] ">
-        <div className="flex flex-col items-center space-y-2 md:items-start md:justify-center">
-          <p className="text-[#969696] capitalize text-sm font-bold">
+      <div className=" mt-20 flex flex-col mx-[10%] px-12 py-5 space-y-5 md:space-y-0 md:py-14 bg-white shadow-lg rounded-2xl md:grid md:grid-cols-4">
+        <div className="flex flex-col items-center">
+          <p className="text-[#969696] capitalize text-sm font-bold md:text-base ">
             IP ADDRESS
           </p>
-          <p className="text-[#2b2b2b] text-xl font-semibold">
+          <p className="text-[#2b2b2b] text-xl font-semibold md:text-2xl">
             192.212.174.101
           </p>
         </div>
-        <div className="flex flex-col items-center space-y-2 md:items-start md:justify-center">
-          <p className="text-[#969696] capitalize text-sm font-bold">
+        <div className="flex flex-col items-center">
+          <p className="text-[#969696] capitalize text-sm font-bold md:text-base ">
             lOCATION
           </p>
-          <p className="text-[#2b2b2b] text-xl font-semibold">
+          <p className="text-[#2b2b2b] text-xl font-semibold md:text-2xl">
             Brooklyn NY 10001
           </p>
         </div>
-        <div className="flex flex-col items-center space-y-2 md:items-start md:justify-center">
-          <p className="text-[#969696] capitalize text-sm font-bold">
+        <div className="flex flex-col items-center  ">
+          <p className="text-[#969696] capitalize text-sm font-bold md:text-base ">
             TIMEZONE
           </p>
-          <p className="text-[#2b2b2b] text-xl font-semibold">UTC -05:00</p>
+          <p className="text-[#2b2b2b] text-xl font-semibold md:text-2xl">
+            UTC -05:00
+          </p>
         </div>
-        <div className="flex flex-col items-center space-y-2 md:items-start md:justify-center">
-          <p className="text-[#969696] capitalize text-sm font-bold">ISP</p>
-          <p className="text-[#2b2b2b] text-xl font-semibold">
+        <div className="flex flex-col items-center">
+          <p className="text-[#969696] capitalize text-sm font-bold md:text-base ">
+            ISP
+          </p>
+          <p className="text-[#2b2b2b] text-xl font-semibold md:text-2xl">
             SpaceX Starlink
           </p>
         </div>
